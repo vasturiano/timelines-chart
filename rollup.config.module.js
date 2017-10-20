5b3c97fe288fd709d2c9eb8da7786b356786dcac
vasco <vastur@gmail.com>
2017-10-20 01:42:51 -0700

    Exclude external dependencies from module bundle

--- rollup.config.module.js
+++ rollup.config.module.js
@@ -1,24 +1,17 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonJs from 'rollup-plugin-commonjs';
 import postCss from 'rollup-plugin-postcss';
 import postCssSimpleVars from 'postcss-simple-vars';
 import postCssNested from 'postcss-nested';
 import babel from 'rollup-plugin-babel';
-import { name, homepage, version } from './package.json';
 
 export default {
     input: 'src/index.js',
     output: [
         {
-            format: 'umd',
-            name: 'TimelinesChart',
-            file: 'dist/timelines-chart.js',
-            sourcemap: true
+            format: 'es',
+            file: 'dist/timelines-chart.mjs'
         }
     ],
     plugins: [
-        resolve(),
-        commonJs(),
         postCss({
             plugins: [
                 postCssSimpleVars(),
@@ -36,6 +29,5 @@ export default {
             ],
             babelrc: false
         })
-    ],
-    banner: `// Version ${version} ${name} - ${homepage}`
+    ]
 };

86d7d5aa45ceddbc6af819e14e0d09bcbd0f897b
vasco <vastur@gmail.com>
2020-03-18 13:32:38 -0700

    Add TS declaration

--- rollup.config.js
+++ rollup.config.js
@@ -4,28 +4,74 @@ import postCss from 'rollup-plugin-postcss';
 import postCssSimpleVars from 'postcss-simple-vars';
 import postCssNested from 'postcss-nested';
 import babel from 'rollup-plugin-babel';
-import { name, homepage, version } from './package.json';
+import { terser } from "rollup-plugin-terser";
+import dts from 'rollup-plugin-dts';
+import { name, homepage, version, dependencies, peerDependencies } from './package.json';
 
-export default {
-  input: 'src/index.js',
-  output: [
-    {
-      format: 'umd',
-      name: 'TimelinesChart',
-      file: `dist/${name}.js`,
-      sourcemap: true,
-      banner: `// Version ${version} ${name} - ${homepage}`
-    }
-  ],
-  plugins: [
-    postCss({
-      plugins: [
-        postCssSimpleVars(),
-        postCssNested()
-      ]
-    }),
-    resolve(),
-    commonJs(),
-    babel({ exclude: 'node_modules/**' })
-  ]
+const umdConf = {
+  format: 'umd',
+  name: 'TimelinesChart',
+  banner: `// Version ${version} ${name} - ${homepage}`
 };
+
+export default [
+  {
+    input: 'src/index.js',
+    output: [
+      { // umd
+        ...umdConf,
+        file: `dist/${name}.js`,
+        sourcemap: true,
+      },
+      { // minify
+        ...umdConf,
+        file: `dist/${name}.min.js`,
+        plugins: [terser({
+          output: { comments: '/Version/' }
+        })]
+      }
+    ],
+    plugins: [
+      postCss({
+        plugins: [
+          postCssSimpleVars(),
+          postCssNested()
+        ]
+      }),
+      resolve(),
+      commonJs(),
+      babel({ exclude: 'node_modules/**' })
+    ]
+  },
+  { // commonJs and ES modules
+    input: 'src/index.js',
+    output: [
+      {
+        format: 'cjs',
+        file: `dist/${name}.common.js`
+      },
+      {
+        format: 'es',
+        file: `dist/${name}.module.js`
+      }
+    ],
+    external: [...Object.keys(dependencies || {}), ...Object.keys(peerDependencies || {})],
+    plugins: [
+      postCss({
+        plugins: [
+          postCssSimpleVars(),
+          postCssNested()
+        ]
+      }),
+      babel()
+    ]
+  },
+  { // expose TS declarations
+    input: 'src/index.d.ts',
+    output: [{
+      file: `dist/${name}.d.ts`,
+      format: 'es'
+    }],
+    plugins: [dts()]
+  }
+];

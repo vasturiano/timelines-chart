458275e3a55e93c96b7f232678ed6aff2c88c18b
vasco <vastur@gmail.com>
2020-11-12 23:15:42 -0800

    Bump deps

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import commonJs from '@rollup/plugin-commonjs';
 import postCss from 'rollup-plugin-postcss';
 import postCssSimpleVars from 'postcss-simple-vars';
 import postCssNested from 'postcss-nested';
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
 import { terser } from "rollup-plugin-terser";
 import dts from 'rollup-plugin-dts';
 import { name, homepage, version, dependencies, peerDependencies } from './package.json';
